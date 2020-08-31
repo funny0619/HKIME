2ce2bdedfa2d65757eab464422b5fd80405edc81
funny0619 <sunnyb@hku.hk>
2020-08-31 15:32:45 +0900

    canto word scraper

--- scraping/dictionary/canto_words_scraper.py
+++ scraping/dictionary/canto_words_scraper.py
@@ -4,32 +4,32 @@ import requests
 from bs4 import BeautifulSoup
 import math
 from time import sleep
-total_levels = 4
+total_levels = 5
 page = 0
 count = 0
 for level in range(1, total_levels+1):
-    URL = "http://www.cantonese.sheik.co.uk/scripts/masterlist.htm?action=onelevel&level=" + \
+    URL = "http://www.cantonese.sheik.co.uk/scripts/wordlist.htm?action=&wordtype=0&level=" + \
         str(level)+"&page="+str(page)
     # page is 0 indexed
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, 'html.parser', from_encoding='utf-8')
-    h4 = soup.find('h4')
-    total_chars = int(h4.text.split()[3])
-    total_pages = math.ceil(total_chars/50)
+    h3 = soup.find('h3')
+    total_chars = int(h3.text.split()[3])
+    total_pages = math.ceil(total_chars/20)
     for page in range(total_pages):
-        URL = "http://www.cantonese.sheik.co.uk/scripts/masterlist.htm?action=onelevel&level=" + \
+        URL = "http://www.cantonese.sheik.co.uk/scripts/wordlist.htm?action=&wordtype=0&level=" + \
             str(level)+"&page="+str(page)
         # page is 0 indexed
         r = requests.get(URL)
         soup = BeautifulSoup(r.content, 'html.parser', from_encoding='utf-8')
-        table = soup.find_all('a', attrs={'class': 'black'})
-        for char in table:
+        table = soup.find_all('a', attrs={'class': 'wordlink'})
+        for word in table:
             dataFile = open("dictionary_list.txt", "a", encoding="utf-8")
-            dataFile.write(char.text[0] + '\n')
+            dataFile.write(word.text + '\n')
             count += 1
         print("level " + str(level) + " page " + str(page))
         page += 1
-        sleep(2)
+        sleep(5)
     print(str(count)+" words in level " + str(level))
     count = 0
     page = 0
